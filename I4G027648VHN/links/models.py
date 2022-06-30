c36377f1adf4ffabf54cba6228288b953f43c9a0
jeph_pico <amadasun.jephthah@gmail.com>
2022-06-30 20:21:03 +0100

    update

--- I4G027648VHN/links/models.py
+++ I4G027648VHN/links/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Link(models.Model):
-    target_url = models.URLField()
-    identifier = models.SlugField(blank=True, unique=True)
+    target_url = models.URLField(max_length=200)
+    identifier = models.SlugField(blank=True, unique=True, max_length=20)
     description = models.CharField(max_length= 200)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
     created_date = models.DateTimeField()
@@ -14,5 +14,6 @@ class Link(models.Model):
 
     def __str__(self) -> str:
         return self.target_url
+
 
 
